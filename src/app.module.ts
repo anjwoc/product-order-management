a1980300d8f8f884ea446279ffcf82bcb0d28d3e
anjwoc <anjwoc@naver.com>
2022-10-05 21:36:23 +0900

    conf: remove payments module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,6 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { OrdersModule } from './orders/orders.module';
 import { ProductsModule } from './products/products.module';
-import { PaymentsModule } from './payments/payments.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { configValidation } from './validations/config.validation';
 import { typeOrmModuleConfig } from './config/typeorm';
@@ -20,7 +19,6 @@ import { typeOrmModuleConfig } from './config/typeorm';
     UsersModule,
     OrdersModule,
     ProductsModule,
-    PaymentsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
