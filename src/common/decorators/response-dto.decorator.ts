b90da231bd50b533ebfc70d6027bda663c0805cd
anjwoc <anjwoc@naver.com>
2022-10-10 21:33:40 +0900

    chore: swagger 누락된 Dto 추가

--- src/common/decorators/response-dto.decorator.ts
+++ src/common/decorators/response-dto.decorator.ts
@@ -1,10 +1,12 @@
 import { applyDecorators, Type } from '@nestjs/common';
 import { ApiExtraModels, ApiOkResponse, getSchemaPath } from '@nestjs/swagger';
+import { OrderDto } from 'src/orders/dto/order.dto';
 import { RequestOrderDto } from 'src/orders/dto/request-order.dto';
 import { ProductDto } from 'src/products/dto/product.dto';
 import { RegisterResponseDto } from 'src/users/dto/register-response.dto';
 import { UserLoginDto } from 'src/users/dto/user-login.dto';
 import { DeleteSuccessDto } from '../dto/delete-success.dto';
+import { PageDto } from '../dto/pagination.dto';
 import { ResponseDto } from '../dto/response.dto';
 import { UpdateSuccessDto } from '../dto/update-success.dto';
 
@@ -18,6 +20,8 @@ export const ApiResponseDto = <TModel extends Type<any>>(model: TModel) => {
       ProductDto,
       UpdateSuccessDto,
       DeleteSuccessDto,
+      OrderDto,
+      PageDto,
     ),
     ApiOkResponse({
       schema: {
